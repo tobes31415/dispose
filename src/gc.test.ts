7803ec445f13038c9445985da8301380b387cec6
Jake Tober <jaket@emids.com>
2023-05-08 16:55:54 -0400

    Refresh dependencies Jest > Mocha docs currently not building

--- src/gc.test.ts
+++ src/gc.test.ts
@@ -1,68 +1,71 @@
 import * as gc from './gc';
+import expect from "expect.js";
 
 describe("gc", () => {
     let foo: {abc: number, sub: { ghi: number }};
     let bar: {def: number};
     let spy:any;
+    let callCount: number = 0;
 
     beforeEach(() => {
         foo = { abc: 123, sub: {ghi : 789}};
         bar = {def: 456};
-        spy = jest.fn();
+        spy = () => {callCount++;}
+        callCount = 0;
     })
 
     it ("invokes the dispose function", () => {
         gc.onDispose(foo, spy);
-        expect(spy).not.toHaveBeenCalled();
+        expect(callCount).to.equal(0);
         gc.dispose(foo);
-        expect(spy).toHaveBeenCalled();
+        expect(callCount).to.equal(1);
     });
 
     it ("returns true when object is disposed", () => {
-        expect(gc.isDisposed(foo)).toBe(false);
+        expect(gc.isDisposed(foo)).to.be(false);
         gc.dispose(foo);
-        expect(gc.isDisposed(foo)).toBe(true);
+        expect(gc.isDisposed(foo)).to.be(true);
     });
 
     it ("Only throw if asserting on disposed object", () => {
-        expect(() => gc.assertNotDisposed(foo)).not.toThrow();
+        expect(() => gc.assertNotDisposed(foo)).to.not.throwError();
         gc.dispose(foo);
-        expect(() => gc.assertNotDisposed(foo)).toThrow();
+        expect(() => gc.assertNotDisposed(foo)).to.throwError();
     });
 
     it("Chains dispose", () => {
         gc.onDisposeChain(foo, bar);
         gc.dispose(foo);
-        expect(gc.isDisposed(bar)).toBe(true);
+        expect(gc.isDisposed(bar)).to.be(true);
     });
 
     it("Deletes Properties", () => {
         gc.onDisposeDeleteProperties(foo);
-        expect(foo.abc).toBeTruthy();
+        expect(foo.abc).to.be.ok();
         gc.dispose(foo);
-        expect(foo.abc as any).toEqual(undefined);
+        expect(foo.abc as any).to.equal(undefined);
     });
 
     it ("Disposes Properties", () => {
         gc.onDisposeDisposeProperties(foo);
-        expect(gc.isDisposed(foo.sub)).toBe(false);
+        expect(gc.isDisposed(foo.sub)).to.be(false);
         gc.dispose(foo);
-        expect(gc.isDisposed(foo.sub)).toBe(true);
+        expect(gc.isDisposed(foo.sub)).to.be(true);
     });
 
     it("Triggers dispose action immedietly if already disposed", () => {
         gc.dispose(foo);
-        expect(spy).not.toHaveBeenCalled();
+        expect(callCount).to.equal(0);
         gc.onDispose(foo, spy);
-        expect(spy).toHaveBeenCalled();    
+        expect(callCount).to.equal(1);
     });
 
     it("Only disposes an object once", () => {
         gc.onDispose(foo, spy);
-        expect(spy).toHaveBeenCalledTimes(0);
+        expect(callCount).to.equal(0);
         gc.dispose(foo);
-        expect(spy).toHaveBeenCalledTimes(1);
+        expect(callCount).to.equal(1);
         gc.dispose(foo);
-        expect(spy).toHaveBeenCalledTimes(1);
+        expect(callCount).to.equal(1);
     });
 });
